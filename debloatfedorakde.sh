0dea0930efecae96442b50051f834d53b6489add
milosmakaveli <milos07.marjanovic@gmail.com>
2025-02-28 23:05:57 +0100

    removing more unnecessary packages

--- debloatfedorakde.sh
+++ debloatfedorakde.sh
@@ -3,3 +3,5 @@
 sudo dnf remove pim* akonadi* kdepim* libreoffice* akregator korganizer kmail kmag kmines kmousetool kmahjongg kmouth kpat kruler krdc krfb ktnef kaddressbook konversation kf5-akonadi-server mariadb mariadb-backup mariadb-common
 
 sudo dnf remove akregator kamoso mediawriter elisa-player kmag kgpg qt5-qdbusviewer kcharselect kcolorchooser dragon kmines kmahjongg kpat kruler kmousetool kmouth kolourpaint konversation krdc kfind kaddressbook kmail kontact korganizer ktnef libreoffice-* kf5-akonadi-*
+
+sudo dnf remove gnome-keyring gnome-desktop3 gnome-desktop4 gnome-abrt
