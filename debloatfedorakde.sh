fa9e69253c776a8af3235c7bf0048189cfa4df28
milosmakaveli <milos07.marjanovic@gmail.com>
2025-02-28 23:03:38 +0100

    converted txt file to shell script

--- debloatfedorakde.sh
+++ debloatfedorakde.sh
@@ -1,6 +1,5 @@
+#!/bin/bash
 
 sudo dnf remove pim* akonadi* kdepim* libreoffice* akregator korganizer kmail kmag kmines kmousetool kmahjongg kmouth kpat kruler krdc krfb ktnef kaddressbook konversation kf5-akonadi-server mariadb mariadb-backup mariadb-common
 
 sudo dnf remove akregator kamoso mediawriter elisa-player kmag kgpg qt5-qdbusviewer kcharselect kcolorchooser dragon kmines kmahjongg kpat kruler kmousetool kmouth kolourpaint konversation krdc kfind kaddressbook kmail kontact korganizer ktnef libreoffice-* kf5-akonadi-*
-
-sudo dnf remove dnfdragora
