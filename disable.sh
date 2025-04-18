85c8819ebc286b1e72e78faa568d56b9b2e4c35e
milosmakaveli <milos07.marjanovic@gmail.com>
2025-04-18 03:55:13 +0200

    fixes for Ubuntu dock

--- disable.sh
+++ disable.sh
@@ -1,2 +1,11 @@
 gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
 gsettings set org.gnome.shell.extensions.dash-to-dock shortcut "['']"
+
+#Minimize by clicking icon in the dock
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action minimize
+
+#Make Nautilus File Manager sort files by type.
+gsettings set org.gnome.nautilus.preferences default-sort-order type
+
+
+
