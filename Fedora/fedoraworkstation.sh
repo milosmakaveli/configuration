2353bc1f3a6b1b70078b1740786cfa330c64721a
Milos Marjanovic <milos07.marjanovic@gmail.com>
2026-07-01 23:45:27 +0200

    added script for debloating fedoraworkstation.sh

---
+++ Fedora/fedoraworkstation.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo dnf remove pim* akonadi* kdepim* libreoffice* akregator korganizer kmail kmag kmines kmousetool kmahjongg kmouth kpat kruler krdc krfb ktnef kaddressbook konversation kf5-akonadi-server mariadb mariadb-backup mariadb-common
+
+sudo dnf remove ibus-libpinyin ibus-libzhuyin ibus-cangjie-* ibus-hangul firefox gnome-boxes decibels showtime
